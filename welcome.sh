fd358bb6c6be891f249722cb19f0b1a71abf5eb8
chima <uniquechima@gmail.com>
2022-02-26 01:51:18 +0000

    new files commit

--- welcome.sh
+++ welcome.sh
@@ -1,7 +1,11 @@
 echo "welcome to training in dev"
 echo " i want to be able to pull a request with this code"
+<<<<<<< Updated upstream
 HEAD
 stashing this
 echo "i wanted to add to your code to make the application user friendly"
 echo "just trying to deploy a feature and maybe solve a merge request."
 echo "welcome"
+=======
+stashing this
+>>>>>>> Stashed changes
